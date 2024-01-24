d5a4af67e37c54ab274cc1c75a0cb9815855ef5a
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-01-24 19:43:54 -0300

    primeira aula de react t1

---
+++ primeira-aula-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"; 
+import Header from "./assets/Header"
+import Main from "./assets/Main"
+
+function App(){
+  return(
+<>
+ <Header />
+<Main />
+</>
+   
+    
+  )
+}
+
+export default App;
